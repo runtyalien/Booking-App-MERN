743e4a06c8e29193392a25ef0e59632ef63164f0
runtyalien <omgadkar@gmail.com>
2023-12-18 12:23:33 +0530

    backend complete. frontend created

---
+++ api/routes/rooms.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GETALL
+router.get("/", getRooms);
+
+export default router;
